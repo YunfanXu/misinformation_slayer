bb41f8df05d7971fdfe508cf5254ad91c4b6e552
Yunfan Xu <yx2277@nyu.edu>
2021-10-08 20:41:28 -0400

    complete the UI

--- popup.js
+++ popup.js
@@ -1,37 +1,26 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
-let changeToWhite = document.getElementById("changeToWhite");
+let tabcontent = document.getElementsByClassName("tabcontent");
+let tablinks = document.getElementsByClassName("tablinks");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+console.log(tabcontent," Clicked");
 
-  console.log("-----tab:",tab);
-  // chrome.scripting.executeScript({
-  //   target: { tabId: tab.id },
-  //   function: openCity,
-  // });
-});
+tablinks.forEach(tablink => {
+  tablink.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    console.log("tab:",tab);
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: openCity,
+    });
   });
-}
+})
 
 function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  console.log(cityName," Clicked");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
