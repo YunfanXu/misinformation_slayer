2e3553d6bc34053910e721415f60eae2c268d2c5
Yunfan Xu <yx2277@nyu.edu>
2021-10-18 00:58:07 -0400

    Developed analysis news

--- background.js
+++ background.js
@@ -1,15 +1,43 @@
-let color = 'black';
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  switch (message.type) {
+    case "bglog":
+      console.log(...message.obj);
+      break;
+    case "postData":
+      let { actionType, currentType, resultLabel, title, url, content } = message.data;
+      if (currentType === 'news') {
+        try {
+          let data = {
+            url,
+            title,
+            content
+          };
+          // var data = {
+          //   url: "http://www.bbc.co.uk/news/world-asia-40909468",
+          //   title: "North Korea: China urges Trump not to worsen situation",
+          //   content: "China's President Xi Jinping has urged Donald Trump..."
+          // }
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
 
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log(
-      `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${oldValue}", new value is "${newValue}".`
-    );
+          data = JSON.stringify(data);
+          // send to the endpoint
+          fetch("http://192.241.156.70:8080/fakebox/check", {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json; charset=utf-8'
+            },
+            body: data
+          }).then(r => r.text())
+            .then(response => {
+              console.log("fetched", response);
+              sendResponse(response);
+
+            });
+        } catch (error) {
+          console.log("Error", error);
+          return false;
+        }
+      }
   }
+  return true;
 });
--- popup.js
+++ popup.js
@@ -1,30 +1,134 @@
 let tabcontent = document.getElementsByClassName("tabcontent");
 let tablinks = document.getElementsByClassName("tablinks");
+let submitButton = document.getElementById('submit_button');
+let newsButton = document.getElementById('news_button');
+let videoButton = document.getElementById('video_button');
+let imageButton = document.getElementById('image_button');
+let contentTitle = document.getElementById("content_title");
+let contentContent = document.getElementById("content_content");
+let actioner = document.getElementsByName("actioner");
+let reportLabel = document.getElementsByName("reportLabel");
+let queryResult = document.getElementById("query_result");
+let selectContainer = document.getElementById("select_container");
+let resultContainer = document.getElementById("result_container");
 
+const tabButton = [newsButton, videoButton, imageButton];
+let currentType = "news";
+let actionType = "report";
+let resultLabel = "real";
 
-console.log(tabcontent," Clicked");
-
-tablinks.forEach(tablink => {
-  tablink.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    console.log("tab:",tab);
-
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: openCity,
-    });
-  });
-})
-
-function openCity(evt, cityName) {
-  console.log(cityName," Clicked");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+var bglog = function () {
+  if (chrome && chrome.runtime) {
+    chrome.runtime.sendMessage({ type: "bglog", obj: [...arguments] });
   }
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
 }
 
+const submitData = () => {
+  let data = {
+    currentType,
+    actionType,
+    resultLabel,
+    url: document.getElementById("url").value,
+    title: document.getElementById("title").value,
+    content: document.getElementById("content").value
+  }
+  bglog("submit Button");
+
+  chrome.runtime.sendMessage({
+    type: "postData",
+    data
+  }, response => {
+    response = JSON.parse(response);
+    bglog("p1121212121opup", response);
+
+    if (response != undefined && response.success) {
+      bglog("popup", response);
+      if (actionType === "report") {
+        alert("Submit Successfully!")
+      } else {
+        queryResult.innerHTML = `url:${response.domain.category} \t Title:${response.title.decsion} \t Content:${response.content.decision} `
+      }
+    }
+    else {
+      bglog("feched failed!")
+    }
+  });
+}
+
+const setSubmitButton = () => {
+  submitButton.addEventListener('click', e => {
+    e.preventDefault();
+    submitData();
+  });
+}
+
+const clearActive = () => {
+  tabButton.forEach(button => button.classList.remove("active"))
+}
+
+const setButtons = () => {
+  tabButton.forEach(button => {
+    button.addEventListener('click', (e) => {
+      clearActive();
+      button.classList.add("active");
+      currentType = button.textContent.toLowerCase();
+      if (currentType === 'news') {
+        showContent();
+      } else {
+        hiddenContent();
+      }
+    })
+  })
+}
+
+const showContent = () => {
+  contentTitle.style.visibility = "visible";
+  contentContent.style.visibility = "visible";
+}
+
+const hiddenContent = () => {
+  contentTitle.style.visibility = "collapse";
+  contentContent.style.visibility = "collapse";
+}
+
+const setActioner = () => {
+  actioner.forEach(item => {
+    item.addEventListener('click', (e) => {
+      actionType = e.target.value;
+      bglog("==actionType====", actionType);
+
+      if (actionType === 'report') {
+        selectContainer.style.zIndex = 2;
+        resultContainer.style.zIndex = 1;
+      } else {
+        selectContainer.style.zIndex = 1;
+        resultContainer.style.zIndex = 2;
+      }
+    });
+  })
+}
+
+const setReportLabel = () => {
+  reportLabel.forEach(item => {
+    item.addEventListener('click', (e) => {
+      if (e.target.checked) {
+        resultLabel = e.target.value;
+        bglog("======", resultLabel);
+      }
+    });
+  })
+}
+const setEventListen = () => {
+  setSubmitButton();
+  setButtons();
+  setActioner();
+  setReportLabel();
+}
+
+
+const main = () => {
+  setEventListen();
+
+}
+
+main();
